ae9b42d013bcbef1357d0b084c46b9d13b506523
coolnimesh43 <coolnimesh43@gmail.cm>
2014-02-13 13:36:53 +0545

    euler: push after adding QuestionNinteen and QuestionEighteen

---
+++ projecteuler/src/projecteuler/QuestionNinteen.java
@@ -0,0 +1,33 @@
+package projecteuler;
+
+import java.util.logging.Logger;
+
+/**
+ * You are given the following information, but you may prefer to do some research for yourself.
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+ */
+/**
+ * @author nimesh
+ *
+ */
+public class QuestionNinteen {
+
+	private static final Logger LOG=Logger.getLogger(QuestionNinteen.class.getName());
+	/**
+	 * This is main class
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LOG.info("Into main class");
+		
+	}
+
+}
